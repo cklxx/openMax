285f18e5cf459d347d310ef549c5e05db6abb6d6
cklxx <q1293822641@gmail.com>
2026-03-25 15:24:27 +0800

    feat: add prime number checker with primes-up-to-50 output

--- prime.py
+++ prime.py
@@ -1,23 +1,16 @@
-"""Prime number checker."""
-
-import sys
 from math import isqrt
 
 
 def is_prime(n: int) -> bool:
-    """Return True if n is prime, False otherwise."""
     if n < 2:
         return False
-    if n < 4:
+    if n == 2:
         return True
-    if n % 2 == 0 or n % 3 == 0:
+    if n % 2 == 0:
         return False
-    for i in range(5, isqrt(n) + 1, 6):
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-    return True
+    return all(n % i != 0 for i in range(3, isqrt(n) + 1, 2))
 
 
 if __name__ == "__main__":
-    num = int(sys.argv[1])
-    print(f"{num} is {'prime' if is_prime(num) else 'not prime'}")
+    primes = [n for n in range(51) if is_prime(n)]
+    print(f"Primes up to 50: {primes}")
