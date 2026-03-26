0e52dd590843ad1ad178504fbf90afaa13ca8135
cklxx <q1293822641@gmail.com>
2026-03-26 16:02:29 +0800

    Add format_message helper to utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
 def capitalize_words(text: str) -> str:
     return " ".join(word.capitalize() for word in text.split())
+
+
+def format_message(template: str, **kwargs: str) -> str:
+    return template.format(**kwargs)
